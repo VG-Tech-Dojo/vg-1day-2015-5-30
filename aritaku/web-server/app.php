f847c8416f20b52c8380b420c7fb179168ae4454
aritaku19@gmail.com <aritaku19@gmail.com>
2015-05-30 15:21:46 +0900

    botと占い機能

--- aritaku/web-server/app.php
+++ aritaku/web-server/app.php
@@ -21,16 +21,32 @@ $app->get('/messages/{id}', function ($id) use ($app) {
 $app->post('/messages', function (Request $request) use ($app) {
     $data = $app->validateRequestAsJson($request);
 
+    //bot機能を追加する
     $username = isset($data['username']) ? $data['username'] : '';
     $body = isset($data['body']) ? $data['body'] : '';
 
-    $createdMessage = $app->createMessage($username, $body, base64_encode(file_get_contents($app['icon_image_path'])));
+    if ($body == "uranai") {
     
-    $username = isset($data['username']) ? $data['username'] : '';
-    $body = isset($data['body']) ? $data['body'] : '';
+    //占い機能を追加
+    $input = array("大吉", "中吉", "小吉");
+    $rand_keys = array_rand($input,1);
 
+        if ($rand_keys == 1) {
+            $body = "大吉";
+        } else if ($rand_keys == 2) {
+            $body = "中吉";
+        } else if ($rand_keys == 3) {
+            $body = "小吉";
+        };
+
+    $createdMessage = $app->createMessage("uranai", $body, base64_encode(file_get_contents($app['icon_image_path'])));
+    
+    } else {
+     
+    $createdMessage = $app->createMessage($username, $body, base64_encode(file_get_contents($app['icon_image_path'])));
     $createdMessage = $app->createMessage("bot", $body, base64_encode(file_get_contents($app['icon_image_path'])));
-
+    
+    };
 
     return $app->json($createdMessage);
 });
