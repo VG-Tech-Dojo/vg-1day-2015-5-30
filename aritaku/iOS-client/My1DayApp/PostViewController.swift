49e4a09d078e0982e7675960c9a69133d9848695
aritaku19@gmail.com <aritaku19@gmail.com>
2015-05-30 14:11:58 +0900

    postViewController変更

--- aritaku/iOS-client/My1DayApp/PostViewController.swift
+++ aritaku/iOS-client/My1DayApp/PostViewController.swift
@@ -14,6 +14,7 @@ protocol PostViewControllerDelagate : NSObjectProtocol {
 
 class PostViewController: UIViewController {
     @IBOutlet weak private var messageTextView: UITextView!
+    @IBOutlet weak private var userName: UITextField!
     weak var delegate: PostViewControllerDelagate?
     // Mission1-2 Storyboard から UITextField のインスタンス変数を追加
 
@@ -33,10 +34,11 @@ class PostViewController: UIViewController {
         self.messageTextView.resignFirstResponder()
         
         let message: String = self.messageTextView.text ?? ""
+        let username: String = self.userName.text ?? ""
         // Mission1-2 UITextField のインスタンス変数から値を取得
         
         // Mission1-2 posetMessage の第2引数に 任意の値を渡す
-        APIRequest.postMessage(message, username: "名前はまだない") {
+        APIRequest.postMessage(message, username: username) {
             [weak self] (data, response, error) -> Void in
             
             self?.delegate?.postViewController(self!, didTouchUpCloseButton: sender)
