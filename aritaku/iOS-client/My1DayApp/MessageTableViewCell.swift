2ec166ca79757ff4c8aad5a2da253e82c4f21a69
aritaku19@gmail.com <aritaku19@gmail.com>
2015-05-30 13:38:38 +0900

    created at label

--- aritaku/iOS-client/My1DayApp/MessageTableViewCell.swift
+++ aritaku/iOS-client/My1DayApp/MessageTableViewCell.swift
@@ -11,17 +11,20 @@ import UIKit
 class MessageTableViewCell: UITableViewCell {
     @IBOutlet weak private var iconImageView: UIImageView!
     @IBOutlet weak private var messageLabel: UILabel!
+    @IBOutlet weak private var dateLabel: UILabel!
     // Mission1-1 UILabel のインスタンス変数を追加
     
     override func prepareForReuse() {
         self.iconImageView.image = nil
         self.messageLabel.text = nil
+        self.dateLabel.text = nil
         // Mission1-1 UILabel のインスタンス変数を初期化
     }
     
     func setupComponentsWithMessage(message: Message) {
         self.iconImageView.image = message.icon
         self.messageLabel.text = message.body
+        self.dateLabel.text = message.date
         // Mission1-1 UILabel のインスタンス変数に created_at の値を代入
     }
 }
